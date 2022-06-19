ca15167209f9616f13ab009f25857b7e0bd0844e
ruxandra <pepicoal@yahoo.com>
2022-06-19 18:26:02 +0300

    all files + js fix on right panel buttons working

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-/*const button1 = document.getElementById("1");
-const button2 = document.getElementById("2");
-const button3 = document.getElementById("3");
-const button4 = document.getElementById("4");
-const button5 = document.getElementById("5");
-const button6 = document.getElementById("6");
-const button7 = document.getElementById("7");
-const button8 = document.getElementById("8");
-const button9 = document.getElementById("9");
-const buttonDot = document.getElementById("dot");
-const button0 = document.getElementById("0");
-const buttonEqual = document.getElementById("equal");
-const buttonDivide = document.getElementById("divide");
-const buttonMultiply = document.getElementById("multiply");
-const buttonAdd = document.getElementById("add");
-const buttonSubstract = document.getElementById("substract");*/
-
 const numberButtons = document.getElementsByClassName("number");
 const clearButton = document.getElementById("clear");
 const clearAllButton = document.getElementById("clearAll");
@@ -60,49 +43,16 @@ equalButton.addEventListener('click', () => {
     setTimeout(() => removeClickedClass(equalButton), 500);
 })
 
-let selectedElements = 0;
-let previousSelectedElement = '';
 Array.from(operationButtons).forEach(operationButton => {
     operationButton.addEventListener('click', () => {
-        selectedElements++;
 
+        Array.from(operationButtons).forEach(element => {
+            if(element.id != operationButton.id) removeClickedClass(element)
+        });
         
-  ///?!!!!!!
-        if (selectedElements > 1) {
-            Array.from(operationButtons).forEach(element => { if(element.id != operationButton.id) removeClickedClass(element)});
-            selectedElements = 1;
-        }
-        operationButton.classList.add("buttonClickedRight");
+        operationButton.classList.toggle("buttonClickedRight");
 
-        if (previousSelectedElement.id === operationButton.id) {
-            operationButton.classList.toggle("buttonClickedRight");
-        }
-        previousSelectedElement = operationButton;
+
     });
         
-       /* if (divide > 1 || multiply > 1 || add > 1 || substract > 1) {
-            operationButton.classList.remove("buttonClickedRight");
-            divide = multiply = add = substract = 0;
-        }
-        if ((divide + multiply + add + substract > 1) && (divide <= 1 && multiply <= 1 && add <= 1 && substract <= 1)) {
-            divide = multiply = add = substract = 0;
-        }
-
-         if (operationButton.id == "divide") {
-            divide++;
-        }
-        if (operationButton.id == "multiply") {
-            multiply++;
-        }
-        if (operationButton.id == "add") {
-            add++;
-        }
-        if (operationButton.id == "substract") {
-            substract++;
-        }
-        
-        
-        console.log(divide, multiply, add, substract  )*/
-
-    
-})
+});
